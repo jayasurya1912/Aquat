96df1e5c631bfb85f2bdb719d74350b1bdafe84b
jayasurya1912 <bsjayasurya1912@gmail.com>
2026-01-23 16:48:31 +0530

    ad

--- script.js
+++ script.js
@@ -79,3 +79,21 @@ function typeEffect() {
 
 typeEffect();
 
+
+  const scrollBtn = document.getElementById("scrollTopBtn");
+
+  window.addEventListener("scroll", () => {
+    if (window.scrollY > 300) {
+      scrollBtn.style.display = "flex";
+    } else {
+      scrollBtn.style.display = "none";
+    }
+  });
+
+  scrollBtn.addEventListener("click", () => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth"
+    });
+  });
+
